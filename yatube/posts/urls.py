d6a2d12c3c886cfa15ce1a6539424a9f251f397f
Nikita Kovalevskiy <koval701@gmail.com>
2021-06-22 00:02:46 +0700

    add metaclass and create group_list

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("group/", views.group_list, name="group_list"),
     path("group/<slug:slug>", views.group_posts, name="group_posts")
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from .models import Group, Post
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
+    latest = Post.objects.all()[:11]
     return render(request, "index.html", {"posts": latest})
 
 
@@ -15,8 +15,14 @@ def group_posts(request, slug):
     данных или возвращает сообщение об ошибке, если объект не найден
     """
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by("-pub_date")[:12]
+    posts = group.posts.all()[:12]
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления условия WHERE group_id = {group_id}
     # posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
+
+
+# view-функция со списком сообществ
+def group_list(request):
+    groups = Group.objects.all()
+    return render(request, "group_list.html", {"groups": groups})
